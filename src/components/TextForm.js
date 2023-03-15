4ee2e859e01db2deb5e6c21d14893cf91815ee12
Sugambaj7 <sugambaj123@gmail.com>
2023-03-15 09:47:17 +0545

    Added Preview and Text Summary, used multiple use state

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,37 +20,72 @@ export default function Textform(props) {
     let newText = "";
     setText(newText);
   };
+  const handleTextSummary = () => {
+    //console.log("Uppercase was clicked" + text);
+    if (text.length === 0) {
+      setCharacters(0);
+      setWords(0);
+      setReadTime(0);
+    } else {
+      let characters = text.length;
+      setCharacters(characters);
+      let words = text.split(" ").length;
+      setWords(words);
+      let readTime = (text.split(" ").length * 5) / 1500;
+      setReadTime(readTime);
+    }
+  };
   const [text, setText] = useState("");
+  const [characters, setCharacters] = useState("");
+  const [words, setWords] = useState("");
+  const [readTime, setReadTime] = useState("");
 
   return (
-    <div>
+    <>
       <div>
-        <h1>{props.heading}</h1>
+        <div>
+          <h1>{props.heading}</h1>
+        </div>
+        <div className="form-floating ">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="mybox"
+          ></textarea>
+          <label htmlFor="mytext">Text Entering Area</label>
+        </div>
+        <div className="my-3">
+          <button className="btn btn-primary" onClick={handleUpClick}>
+            Convert To UpperCase
+          </button>
+        </div>
+        <div className="my-3">
+          <button className="btn btn-primary" onClick={handleLowClick}>
+            Convert To Lowercase
+          </button>
+        </div>
+        <div className="my-3">
+          <button className="btn btn-primary" onClick={handleclear}>
+            Clear
+          </button>
+        </div>
       </div>
-      <div className="form-floating ">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="mybox"
-        ></textarea>
-        <label htmlFor="mytext">Text Entering Area</label>
+
+      <div className="my-5">
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
-      <div className="my-3">
-        <button className="btn btn-primary" onClick={handleUpClick}>
-          Convert To UpperCase
+
+      <div className="my-5">
+        <h2>Your text summary</h2>
+        <button className="btn btn-primary" onClick={handleTextSummary}>
+          Generate
         </button>
+        <p>Number of characters in your text: {characters}</p>
+        <p>Number of words in your text: {words}</p>
+        <p>People can read the entered text in average: {readTime} minutes</p>
       </div>
-      <div className="my-3">
-        <button className="btn btn-primary" onClick={handleLowClick}>
-          Convert To Lowercase
-        </button>
-      </div>
-      <div className="my-3">
-        <button className="btn btn-primary" onClick={handleclear}>
-          Clear
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
